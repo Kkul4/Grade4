061b9bca2033711efaae23d31fbcd0c8d7672b7f
Kkul4 <ct.fyture440@gmail.com>
2024-05-02 14:49:46 +0700

    input number

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        sc.close();
+        System.out.println("input number: ");
+        int numder = sc.nextInt();
 
+        if (numder<49) {
+            System.out.println("grade F"+numder);
+        }
+        else if (numder>49 && numder<60) {
+            System.out.println("grade D"+numder);
+        }
+//        if (numder<59) {
+//            System.out.println("grade D"+numder);
+//        }
+        else if (numder>59 && numder<70) {
+            System.out.println("grade C"+numder);
+        }
+//        if (numder<69) {
+//            System.out.println("grade D"+numder);
+//        }
+        else if (numder>69 && numder<80) {
+            System.out.println("grade B" + numder);
+        }
+//        if (numder<79) {
+//            System.out.println("grade B"+numder);
+//        }
+        else if (numder>79 && numder<100) {
+            System.out.println("grade A"+numder);
+        }
     }
 }
